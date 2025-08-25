818704a6f65de8f0b56677b0d1ee108005f53843
Crisgn12 <cristopher.gonzalez.nunez@est.una.ac.cr>
2025-08-24 19:04:13 -0600

    Pagina Welcome

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 
 const Welcome = () => {
     const router = useRouter();
@@ -8,10 +8,28 @@ const Welcome = () => {
 
   return (
     <View className='flex-1 items-center justify-center'>
-      <Text>welcome</Text>
-      <Pressable onPress={() => router.push('/auth/login')}>
-        <Text className='text-blue-500'>Go to Login</Text>
-      </Pressable>
+      <Image 
+        source={require('../assets/images/HireMatch-Logo.png')} 
+        className='size-60'
+      />
+      <View className='items-center px-10 '>
+        <Text className='justify-center text-lg text-center'>Accede a cientos de oportunidades laborales en un solo lugar</Text>
+      </View>
+
+      <View className='w-full px-10 mt-10 gap-4'>
+        <Pressable className="border border-primary bg-primary rounded-xl w-full mb-3" onPress={() => router.push('/auth/login')}>
+          <Text className="text-center text-white p-3 rounded-xl font-semibold">
+            Iniciar Sesión
+          </Text>
+        </Pressable>
+
+        <Pressable className="border border-primary rounded-xl w-full" onPress={() => router.push('/auth/register')}>
+          <Text className="text-center text-primary p-3 rounded-xl font-semibold">
+            Regístrate
+          </Text>
+        </Pressable>
+
+      </View>
     </View>
   )
 }
